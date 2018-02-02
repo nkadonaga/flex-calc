eeb33f80a749ecb24ba284641e969783dcc8e48d
Natalie Kadonaga <nat.kadonaga@gmail.com>
2018-02-02 15:27:26 -0800

    Store created products in db.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,20 @@
 class ProductsController < ApplicationController
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:product].inspect
+    @product = Product.new(product_params)
+
+    @product.save
+    redirect_to @product
   end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end 
 end
